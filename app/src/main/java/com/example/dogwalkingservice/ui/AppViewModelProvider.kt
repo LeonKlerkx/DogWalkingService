8085244c91d29c32f92bef290bb77a4cd4b93966
Leon Klerkx <leonklerkx@gmail.com>
2025-06-02 17:03:02 +0200

    ViewModelProvider aangemaakt zodat de viewModel weet welke Factory bij welk scherm hoort

---
+++ app/src/main/java/com/example/dogwalkingservice/ui/AppViewModelProvider.kt
@@ -0,0 +1,30 @@
+package com.example.dogwalkingservice.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.dogwalkingservice.DogWalkingServiceApplication
+import com.example.dogwalkingservice.ui.login.LoginViewModel
+
+/**
+ * Provides Factory to create instance of ViewModel for the entire Inventory app
+ */
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        // Initializer for LoginViewModel.
+        initializer {
+            LoginViewModel(
+                gebruikersRepository = dogWalkingServiceApplication().container.gebruikersRepository
+            )
+        }
+    }
+}
+
+/**
+ * Extension function to queries for [Application] object and returns an instance of
+ * [DogWalkingServiceApplication].
+ */
+fun CreationExtras.dogWalkingServiceApplication(): DogWalkingServiceApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as DogWalkingServiceApplication)
